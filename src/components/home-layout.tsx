756f3242d11030d1b2e5507d4bb432e769a67c5d
Gerardusdavidbayuaji <gerardusdavidbayu@gmail.com>
2023-11-24 12:55:22 +0700

    feat: update state management and deploy

--- src/components/home-layout.tsx
+++ src/components/home-layout.tsx
@@ -8,7 +8,7 @@ const HomeLayout: React.FC = () => {
         <NavbarHome/>
         <div className="container grow mx-auto py-10 px-10 flex">
           <div className="w-1/2 flex flex-col justify-center place-items-center">
-            <img className="w-4/6 h-auto flex justify-center" src="/assets/imagehome.png" alt="cover home page"/>
+            <img className="w-auto h-64" src="/assets/imagehome.png" alt="cover home page"/>
             <p className="mt-6 font-roboto text-lg" style={{ color: '#0A4D68'}}><span className='text-4xl font-semibold tracking-tight font-roboto' style={{ color: '#05BFDB' }}>Library App </span> is a platform that provides access <br/> to the latest technology resources and information, <br/>including books, journals, articles, and tutorials, <br/> to support technology development.</p>
           </div>
           <div className="w-1/2 flex justify-center place-items-center">
